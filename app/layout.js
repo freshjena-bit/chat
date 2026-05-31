b7f2dfd78e6dad89e57103d7133fb386a1ab5964
freshjena-bit <freshjena@gmail.com>
2026-06-01 01:00:30 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "DadGPT Chat",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
